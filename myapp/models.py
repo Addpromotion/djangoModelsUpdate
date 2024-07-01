3df83478eb5d908769c8be184584be01b9a2910c
Addpromotion <5877187@gmail.com>
2024-07-01 08:56:48 +0300

    Added beautiful pages for customers, products, and orders. Implemented positive value validation for product price and quantity.

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Customer(models.Model):
@@ -13,8 +14,8 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
     date_added = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='product_photos/', null=True, blank=True)
 
